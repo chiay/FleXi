376da99457f22729c2d8411814e8689cfc038145
Unknown <chiay@oregonstate.edu>
2020-01-23 03:15:55 -0800

    Complete basic listview integration

--- components/SettingScreen.js
+++ components/SettingScreen.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Button } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -7,37 +7,23 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 export default class SettingScreen extends React.Component {
    render() {
      return (
-      <View style={styles.container}>
-      <SafeAreaView style={styles.container}>
-         <View style={{ backgroundColor: "#364f6b", borderBottomStartRadius: 50, borderBottomEndRadius: 50, height: 200 }}>
-            <TouchableOpacity style={styles.bars} onPress={this.props.navigation.openDrawer}>
-               <FontAwesomeIcon icon={faBars} size={24} color="#364f6b" />
-            </TouchableOpacity>
-         </View>
-         <View style={{ flex:1, alignItems: "center", justifyContent: "center" }}>
-         <Button
-         onPress={() => this.props.navigation.navigate('Setting')}
-         title="Go to Setting"
-         />
-         </View>
-      </SafeAreaView>
-   </View>
-       
+       <View style={ styles.container }>
+          <FlatList
+             data={[
+                {key: 'Rules'},
+                ]}
+             renderItem={({ item }) => <Text style={ styles.item }>{ item.key }</Text>}
+          />
+       </View>
      );
    }
  }
 
-const styles = StyleSheet.create({
+ const styles = StyleSheet.create({
    container: {
-      flex: 1,
-      backgroundColor: "#f0f0f0"
+     flex: 1,
    },
-   bars: {
-      alignItems: "flex-start",
-      margin: 16
+   item: {
+      fontSize: 18,
    },
-   text: {
-      fontSize: 20,
-      color: "#161924"
-   }
-})
+ });
